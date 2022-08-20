8dcea484ad21a5f1653c652736c61bad12fcab9a
Sandeep Kumar <sandepten@gmail.com>
2022-08-20 18:55:48 +0530

    long time no see

--- pages/blogpost/[Slug].js
+++ pages/blogpost/[Slug].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 const Slug = ({ blogs }) => {
   const router = useRouter();
   const Slug = router.query.Slug;
+  console.log(Slug)
   return (
     <div className="text-white">
       <p className="text-3xl font-bold text-center my-4">{blogs.title}</p>
@@ -14,7 +15,7 @@ const Slug = ({ blogs }) => {
 
 export async function getServerSideProps({params}) {
   const res = await fetch(
-    `http://localhost:3000/api/blog?id=${params.Slug}`
+    `api/blog?id=${params.Slug}`
   );
   const blogs = await res.json();
   return { props: { blogs } };
